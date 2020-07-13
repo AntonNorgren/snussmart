2d849c808b9dc2360646352effc31ab647777f1a
antonnorgren <56293438+AntonNorgren@users.noreply.github.com>
2020-07-13 13:40:40 +0200

    DELETE redundant files

--- snussmart/snussmart/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace snussmart.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- snussmart/snussmart/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace snussmart.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
