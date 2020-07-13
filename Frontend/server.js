aec0a60f803c32a1bd6d4c51fb0a1472637d672b
emilundg <lundgrenemil@hotmail.com>
2020-07-13 17:00:04 +0200

    ADD specific view

--- Frontend/server.js
+++ Frontend/server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/snus/:snusId', function (req, res) {
+    // res.send(req.params)
+    res.sendFile(path.join(__dirname + '/specific-snuff.html'));
+});
+
 app.listen(8000, () => {
     console.log('Example app listening on port 8000!')
 });
